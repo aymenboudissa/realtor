7929b8056f233c74a4fc0a79e7706cbd2168a3e0
aymenboudissa <a.boudissa@ensh.dz>
2022-10-31 19:11:42 +0100

    another commit

---
+++ components/slider/Slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const slider = () => {
+  return (
+    <div className="container__slider">
+      <div className="home__title">Elegant,spacious three bedroom</div>
+      <div className="homer__price">$1975 / month</div>
+    </div>
+  );
+};
+
+export default slider;
